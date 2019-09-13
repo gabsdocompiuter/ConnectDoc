1a6a4eb6e8a929c096ef1e655dd0f92ba01101bd
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-09-12 21:15:57 -0300

    Criado projeto da tela de login

---
+++ frontend/js/login.js
@@ -0,0 +1,30 @@
+function addLoginField(masterDiv, placeholder, type){
+    let label = document.createElement('label');
+    label.innerText = placeholder;
+
+    let input = document.createElement('input');
+    input.type = type;
+
+    let field = document.createElement('div');
+    field.setAttribute('class', 'loginField')
+    field.appendChild(label);
+    field.appendChild(input);
+    
+    field.onclick = () => {
+        field.setAttribute('class', 'loginField on');
+        input.focus();
+    };
+    
+    input.addEventListener("blur", () => {
+        if(!input.innerText){
+            field.setAttribute('class', 'loginField');
+        }
+    });
+    
+    let dad = document.getElementById(masterDiv);
+    dad.appendChild(field);
+}
+
+
+addLoginField('loginArea', 'Login', 'text')
+addLoginField('loginArea', 'Senha', 'password')
