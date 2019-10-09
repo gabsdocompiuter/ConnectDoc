912722ec72170cec43c9d5b2c1a49e4bc5c57414
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-10-09 02:23:59 -0300

    Update index.php

--- frontend/index.php
+++ frontend/index.php
@@ -6,4 +6,34 @@
 
     $p = array_key_exists('p', $_GET) ? $_GET['p'] : "home";
 
-    include 'includes/login.php';
+    //include 'includes/login.php';
+
+    session_start();
+    $usuarioLogado = ISSET($_COOKIE['user']) || ISSET($_SESSION['user']) ? true : false;
+
+    if($usuarioLogado){
+        //Se tentar logar, manda pra home
+        if($p == "login"){
+            header("Location: home");
+        }
+
+        //Se tentar abrir uma página não existente, vai pro 404
+        if(paginaValida($p)) {
+            include('dash.php');
+            return;
+        }
+        else if($p == 'logout'){
+            include('logout.php');
+            return;
+        }
+        else{
+            header("Location: 404");
+        }
+    }
+    else if($p == "login"){
+        include('includes/login.php');
+        return;
+    }
+    else{
+        header("Location: login");
+    }
