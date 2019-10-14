48d786ef819e7090ea93def7b83067d8e6022895
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-10-13 21:03:17 -0300

    Cria método para retornar json de resposta

--- backend/config.php
+++ backend/config.php
@@ -1,6 +1,14 @@
 <?php
-if(!isset($_SESSION)) 
-{ 
+
+function getJsonResponse($success, $message){
+    $response = array(
+        'success' => $success,
+        'message' => $message
+    );
+
+    return json_encode($response);
+}
+
+if(!isset($_SESSION)){ 
     session_start(); 
-} 
-?>
+}
