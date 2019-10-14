54f5e523f9a7c4cdf6ec4b12adfd3c4ce3f28ad3
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-10-13 22:29:44 -0300

    Altera credenciais do banco

--- backend/init.php
+++ backend/init.php
@@ -3,10 +3,10 @@
 define('BASE_PATH', dirname(__FILE__));
  
 // credenciais de acesso ao MySQL
-define('MYSQL_HOST', 'localhost');
-define('MYSQL_USER', 'connectdoc');
-define('MYSQL_PASS', '7%H4ibRD0B@h');
-define('MYSQL_DBNAME', 'connectdoc');
+define('MYSQL_HOST', 'mysql.monteiro.dev');
+define('MYSQL_USER', 'monteiro23');
+define('MYSQL_PASS', 'ulbra2019');
+define('MYSQL_DBNAME', 'monteiro23');
  
 // configurações do PHP
 ini_set('display_errors', true);
