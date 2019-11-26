26012dbd32deec2ed626008501e4d0249d643f15
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-11-26 10:56:21 -0300

    Ajusta calendario

--- frontend/js/calendario.js
+++ frontend/js/calendario.js
@@ -1,27 +1,43 @@
-function montaMeses(){
-    adicionaMes('Jan');
-    adicionaMes('Fev');
-    adicionaMes('Mar');
-    adicionaMes('Abr');
-    adicionaMes('Mai');
-    adicionaMes('Jun');
-    adicionaMes('Jul');
-    adicionaMes('Ago');
-    adicionaMes('Set');
-    adicionaMes('Out');
-    adicionaMes('Nov');
-    adicionaMes('Dez');
+const selecionado = {
+    mes: '',
+    dia: '',
+}
+
+const date = new Date();
+const monthNames = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+];
+
+function montaMeses(mesSelecionado){
+    const lista = document.getElementById('meses_lista');
+    lista.innerHTML = '';
+
+    monthNames.map(mes => adicionaMes(mes.substring(0, 3)));
 
     function adicionaMes(mes){
         function atualizaDias(){
+            montaMeses(mes);
             montaDias(mes);
         }
 
         const item = document.createElement('li');
+        item.setAttribute('id', `month_${mes}`)
         item.innerText = mes;
         item.onclick = atualizaDias;
+
+        if(mes == mesSelecionado) item.setAttribute('class', 'selecionado');
         
-        const lista = document.getElementById('meses_lista');
         lista.appendChild(item);
     }
 }
@@ -36,17 +52,28 @@ function montaDias(mes){
         .then(response => {
             response.map(dia => {
                 function atualizaAgenda(){
-                    alert(dia);
+                    selecionado.mes = mes;
+                    selecionado.dia = dia;
+
+                    montaDias(mes, dia);
+                    // alert(dia);
                 }
 
                 const item = document.createElement('li');
+                item.setAttribute('id', `day_${dia}`)
                 item.innerText = dia;
                 item.onclick = atualizaAgenda;
+
+                if(dia == selecionado.dia && mes == selecionado.mes)
+                    item.setAttribute('class', 'selecionado');
                 
                 lista.appendChild(item);
             })
         })
 }
 
-montaMeses();
-montaDias('fev');
+selecionado.mes = monthNames[date.getMonth()].substring(0, 3); 
+selecionado.dia = date.getDate();
+
+montaMeses(selecionado.mes);
+montaDias(selecionado.mes);
