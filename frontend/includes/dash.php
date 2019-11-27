9186fcfbacfb7a968cc8d61d7c4359892e62e2f7
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-11-27 01:18:45 -0300

    Remove leitura da agenda do PHP

--- frontend/includes/dash.php
+++ frontend/includes/dash.php
@@ -19,60 +19,14 @@
     <body>
         <?php include 'menu.php' ?>
         
-        <div class="container">
+        <div class="container" id="container">
             <div class="calendario">
                 <ul id="meses_lista" class="meses"></ul>
 
                 <ul id="dias_lista" class="dias"></ul>
             </div>
 
-            <?php
-                $json_file = json_decode(file_get_contents("http://localhost/backend/users/medicos"));
-                for($i = 0; $i < count($json_file); $i++) {
-            ?>
-                    <div class="medico">
-                        <div class="medico info">
-                            <h1><?= $json_file[$i]->{'nome'}?></h1>
-                            <h3><?= $json_file[$i]->{'descricao'}?></h3>
-                        </div>
-                        <div class="medico agenda">
-                            <div class="linha">
-                                <div class="horario titulo">Horário</div>
-                                <div class="paciente titulo">Paciente</div>
-                            </div>
-
-                            <?php
-                                $medId = $json_file[$i]->{'id'};
-                                if(isset($_GET['dia'])){
-                                $dia = $_GET['dia'];
-                                $mes = $_GET['mes'];
-                                $agenda = json_decode(file_get_contents("http://localhost/backend/agenda/consultas/$medId/dia/$dia/mes/$mes"));
-                                }else{
-                                    $agenda = json_decode(file_get_contents("http://localhost/backend/agenda/consultas/$medId"));
-                                }
-                                for($x = 0; $x < count($agenda); $x++) {
-                                    $horario = $agenda[$x]->{'horario'};
-                                    $horario = date("H:i",strtotime($horario));
-
-                                    $paciente = $agenda[$x]->{'nomePaciente'};
-                                    
-                                    if(!ISSET($agenda[$x]->{'nomePaciente'})){
-                                        $horario = "<a href='#'>" . $horario . "</a>";
-                                        $paciente = "<a href='#'> — </a>";
-                                    }
-                            ?>
-                                    <div class="linha">
-                                        <div class="horario"><?= $horario ?></div>
-                                        <div class="paciente"><?= $paciente ?></div>
-                                    </div>
-                            <?php
-                                }
-                            ?>
-                        </div>
-                    </div>
-            <?php
-                }
-            ?>
+            
         </div>
         <script src="frontend/js/calendario.js"></script>
     </body>
