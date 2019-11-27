374cd17e11066232c28c2a3a4ad3f2168e0e8aad
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-11-27 13:19:03 -0300

    Cria tela para cadastrar

---
+++ frontend/includes/cadastrar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" type="text/css" href="frontend/css/style.css">
+        <!-- <link rel="stylesheet" type="text/css" href="frontend/css/modal.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="frontend/css/consultas.css"> -->
+
+        <title>ConnectDoc</title>
+    </head>
+
+    <body>
+        <?php include 'menu.php' ?>
+        
+        <div class="container" id="container">
+            
+            
+        </div>
+        
+        <script src="frontend/js/padrao.js"></script>
+
+        <script src="frontend/js/core/axios.min.js"></script>
+        <script src="frontend/js/formAjax.js"></script>
+        <!-- <script src="frontend/js/calendario.js"></script> -->
+
+        <script>
+            ajustaMenu('cadastrar');
+        </script>
+    </body>
+</html>
