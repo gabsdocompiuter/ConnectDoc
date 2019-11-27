b74f0ee674fd7756c9aa1dfff2bbb5cf278b655d
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-11-27 01:38:41 -0300

    Monta a agenda pelo JS

--- frontend/js/calendario.js
+++ frontend/js/calendario.js
@@ -1,9 +1,3 @@
-const selecionado = {
-    mes: '',
-    dia: '',
-}
-
-const date = new Date();
 const monthNames = [
     "Janeiro",
     "Fevereiro",
@@ -19,60 +13,167 @@ const monthNames = [
     "Dezembro"
 ];
 
-function montaMeses(mesSelecionado){
-    const lista = document.getElementById('meses_lista');
-    lista.innerHTML = '';
+const selecionado = {
+    ano: 2019,
+    mes: '',
+    dia: '',
 
-    monthNames.map(mes => adicionaMes(mes.substring(0, 3)));
+    data: () => {
+        const ano = selecionado.ano;
+        const dia = selecionado.dia;
+        let data = 0;
 
-    function adicionaMes(mes){
-        function atualizaDias(){
-            montaMeses(mes);
-            montaDias(mes);
-        }
+        monthNames.map((name, index) => {
+            if(name.substring(0, 3) == selecionado.mes){
+                const mes = index + 1;
+                data = `${ano}${mes}${dia}`;
+            }
+        });
 
-        const item = document.createElement('li');
-        item.setAttribute('id', `month_${mes}`)
-        item.innerText = mes;
-        item.onclick = atualizaDias;
-
-        if(mes == mesSelecionado) item.setAttribute('class', 'selecionado');
-        
-        lista.appendChild(item);
+        return data;
     }
 }
 
-function montaDias(mes){
-    const lista = document.getElementById('dias_lista');
-    lista.innerHTML = '';
-    const endpoint = `http://localhost/backend/dias/${mes}`;
-    fetch(endpoint)
-        .then(response => response.json())
-        .then(response => {
-            response.map(dia => {
-                function atualizaAgenda(){
-                    selecionado.mes = mes;
-                    selecionado.dia = dia;
+function montaCalendario(){
+    const date = new Date();
+    
 
-                    montaDias(mes, dia);
-                    // alert(dia);
-                }
+    function montaMeses(mesSelecionado){
+        const lista = document.getElementById('meses_lista');
+        lista.innerHTML = '';
+    
+        monthNames.map(mes => adicionaMes(mes.substring(0, 3)));
+    
+        function adicionaMes(mes){
+            function atualizaDias(){
+                montaMeses(mes);
+                montaDias(mes);
+            }
+    
+            const item = document.createElement('li');
+            item.setAttribute('id', `month_${mes}`)
+            item.innerText = mes;
+            item.onclick = atualizaDias;
+    
+            if(mes == mesSelecionado) item.setAttribute('class', 'selecionado');
+            
+            lista.appendChild(item);
+        }
+    }
 
-                const item = document.createElement('li');
-                item.setAttribute('id', `day_${dia}`)
-                item.innerText = dia;
-                item.onclick = atualizaAgenda;
-
-                if(dia == selecionado.dia && mes == selecionado.mes)
-                    item.setAttribute('class', 'selecionado');
-                
-                lista.appendChild(item);
+    function montaDias(mes){
+        const lista = document.getElementById('dias_lista');
+        lista.innerHTML = '';
+        const endpoint = `http://localhost/backend/dias/${mes}`;
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                response.map(dia => {
+                    function atualizaAgenda(){
+                        selecionado.mes = mes;
+                        selecionado.dia = dia;
+    
+                        montaDias(mes, dia);
+                        // alert(dia);
+                    }
+    
+                    const item = document.createElement('li');
+                    item.setAttribute('id', `day_${dia}`)
+                    item.innerText = dia;
+                    item.onclick = atualizaAgenda;
+    
+                    if(dia == selecionado.dia && mes == selecionado.mes)
+                        item.setAttribute('class', 'selecionado');
+                    
+                    lista.appendChild(item);
+                })
             })
-        })
+    }
+
+    selecionado.mes = monthNames[date.getMonth()].substring(0, 3); 
+    selecionado.dia = date.getDate();
+
+    montaMeses(selecionado.mes);
+    montaDias(selecionado.mes);
 }
 
-selecionado.mes = monthNames[date.getMonth()].substring(0, 3); 
-selecionado.dia = date.getDate();
+function montaAgenda(){
+    function montaMedicos(){
+        const endpoint = `http://localhost/backend/users/medicos`;
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                response.map(medico => {
+                    const medNome = document.createElement('h1');
+                    medNome.innerText = medico.nome;
 
-montaMeses(selecionado.mes);
-montaDias(selecionado.mes);
+                    const medDesc = document.createElement('h3');
+                    medDesc.innerText = medico.descricao;
+
+                    const medInfo = document.createElement('div');
+                    medInfo.setAttribute('class', 'medico info');
+                    medInfo.appendChild(medNome);
+                    medInfo.appendChild(medDesc);
+
+                    const horarioTitulo = document.createElement('div');
+                    horarioTitulo.setAttribute('class', 'horario titulo');
+                    horarioTitulo.innerText = 'Horário';
+
+                    const pacienteTitulo = document.createElement('div');
+                    pacienteTitulo.setAttribute('class', 'paciente titulo');
+                    pacienteTitulo.innerText = 'Paciente';
+
+                    const linha = document.createElement('div');
+                    linha.setAttribute('class', 'linha');
+                    linha.appendChild(horarioTitulo);
+                    linha.appendChild(pacienteTitulo);
+
+                    const medAgenda = document.createElement('div');
+                    medAgenda.setAttribute('class', 'medico agenda');
+                    medAgenda.appendChild(linha);
+
+                    leHorarios(medico.id, medAgenda);
+
+                    const divMedico = document.createElement('div');
+                    divMedico.setAttribute('class', 'medico');
+                    divMedico.appendChild(medInfo);
+                    divMedico.appendChild(medAgenda);
+
+                    const container = document.getElementById('container');
+                    container.appendChild(divMedico);
+
+                })
+            });
+    }
+
+    function leHorarios(medId, divAgenda){
+        const endpoint = `http://localhost/backend/agenda/consultas/${selecionado.data()}/${medId}`;
+        // console.log(endpoint);
+        
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                response.map(agendamento => {
+                    const horario = document.createElement('div');
+                    horario.setAttribute('class', 'horario');
+                    horario.innerText = agendamento.horario;
+
+                    const paciente = document.createElement('div');
+                    paciente.setAttribute('class', 'paciente');
+                    paciente.innerText = agendamento.paciente ? agendamento.paciente : '—';
+
+                    const linha = document.createElement('div');
+                    linha.setAttribute('class', 'linha');
+                    linha.appendChild(horario);
+                    linha.appendChild(paciente);
+
+                    divAgenda.appendChild(linha);
+                })
+            });
+    }
+
+    montaMedicos();
+}
+
+montaCalendario();
+montaAgenda();
