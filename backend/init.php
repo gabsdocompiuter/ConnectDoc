6c3ad8cd50b411a3ed01d569bfb8a397efae6cf9
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-12-02 15:53:00 -0300

    Update init.php

--- backend/init.php
+++ backend/init.php
@@ -3,10 +3,15 @@
 define('BASE_PATH', dirname(__FILE__));
  
 // credenciais de acesso ao MySQL
-define('MYSQL_HOST', 'mysql.monteiro.dev');
-define('MYSQL_USER', 'monteiro23');
-define('MYSQL_PASS', 'ulbra2019');
-define('MYSQL_DBNAME', 'monteiro23');
+// define('MYSQL_HOST', 'mysql.monteiro.dev');
+// define('MYSQL_USER', 'monteiro23');
+// define('MYSQL_PASS', 'ulbra2019');
+// define('MYSQL_DBNAME', 'monteiro23');
+
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'imed');
+define('MYSQL_PASS', 'imed10741961');
+define('MYSQL_DBNAME', 'connectdoc');
  
 // configurações do PHP
 ini_set('display_errors', true);
